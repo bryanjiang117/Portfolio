9d83d8b6d07ffc2f57cbc5dc3faf12e9663ebae9
Bryan Jiang <bryan@Bryans-MacBook-Air.local>
2024-09-15 23:04:59 -0400

    fix: fixed production build not working (removed HMR)

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,7 @@
-import { dev } from '$app/environment';
+// import { dev } from '$app/environment';
 
 // hot-reload (only during dev)
-export const csr = dev;
+// export const csr = dev;
 
 // prerender since the page is static => faster loading and better SEO
 export const prerender = true;
